d4d8c5bb6ffece2c48e07e7ea121a2df49695ab7
Nicholas Sazy <36042141+nsazy@users.noreply.github.com>
2025-02-23 20:14:07 -0500

    Delete library directory

--- library/locallibrary/catalog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('addbook/', views.addbook, name='addbook'),
-    path('addbook/addbookrecord/', views.addbookrecord, name='addbookrecord'),
-]
